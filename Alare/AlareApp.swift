6e3b5e333a5aeaf322a2b95d2f43df9aa416356b
Cizzuk <dropcat@cizzuk.net>
2026-02-18 14:59:20 +0900

    Create new Xcode project

---
+++ Alare/AlareApp.swift
@@ -0,0 +1,32 @@
+//
+//  AlareApp.swift
+//  Alare
+//
+//  Created by Cizzuk on 2026/02/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AlareApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
