7411cf6d8edcf9806c26e0a7dc6d1bdff0c95e09
Cizzuk <dropcat@cizzuk.net>
2026-02-19 18:14:16 +0900

    Update AlarmData.swift

--- Alare/Models/AlarmData.swift
+++ Alare/Models/AlarmData.swift
@@ -5,7 +5,7 @@
 //  Created by Cizzuk on 2026/02/18.
 //
 
-import Foundation
+import AlarmKit
 import SwiftData
 
 @Model
@@ -21,4 +21,12 @@ final class AlarmData {
     var snoozeIntervalMinutes: Int = 9 // 5 - 15
     
     init() {}
+    
+    // Register next alarms
+    func register() async {
+        await unregister()
+    }
+    
+    // Unregister all registered alarms
+    func unregister() async {}
 }
