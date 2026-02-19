212a30299727e62a0edfa15fa3cbe08026b13e38
Cizzuk <dropcat@cizzuk.net>
2026-02-20 00:02:36 +0900

    Update AlarmData.swift

--- Alare/Models/AlarmData.swift
+++ Alare/Models/AlarmData.swift
@@ -14,7 +14,7 @@ struct AlarmData: Codable {
     var registeredAlarms: [UUID] = []
     var hour: Int = 9 // 0 - 23
     var minute: Int = 0 // 0 - 59
-    var repeats: [Locale.Weekday] = [] // Empty = No repeat
+    var repeats: Set<Locale.Weekday> = [] // Empty = No repeat
     var sound: AlarmSound = AlarmSound.default
     var snoozeIntervalMinutes: Int = 9 // 5 - 15
 }
