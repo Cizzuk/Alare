a13ff6fd68085f4488af9ebf38700ab82eead5ec
Cizzuk <dropcat@cizzuk.net>
2026-02-20 10:54:08 +0900

    Update AlertPresets.swift

--- Alare/Models/AlertPresets.swift
+++ Alare/Models/AlertPresets.swift
@@ -62,6 +62,7 @@ struct OpenAppIntent: LiveActivityIntent {
     static var isDiscoverable = false
     
     func perform() throws -> some IntentResult {
+        Task { await AlarmManager.shared.snooze() }
         return .result()
     }
 }
