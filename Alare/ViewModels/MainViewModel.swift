6cf74e0b7ffb5c8348d0be8caed0de17be14335e
Cizzuk <dropcat@cizzuk.net>
2026-02-21 19:48:46 +0900

    Add date validation

--- Alare/ViewModels/MainViewModel.swift
+++ Alare/ViewModels/MainViewModel.swift
@@ -13,6 +13,15 @@ class MainViewModel: ObservableObject {
     
     @Published var draft: AlarmSettings = AlarmSettings() {
         didSet {
+            // Date validation
+            let next = AlarmSupport.cutSeconds(draft.next)
+            let now = AlarmSupport.cutSeconds(Date())
+            if next <= now {
+                // Add 1 day
+                draft.next = Calendar.current.date(byAdding: .day, value: 1, to: draft.next) ?? draft.next
+            }
+            
+            // Update
             Task { await alarm.update(draft) }
         }
     }
