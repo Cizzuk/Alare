100b6dc414955a251ea7858a3098a8569af0343f
Cizzuk <dropcat@cizzuk.net>
2026-02-23 16:58:28 +0900

    Update StopAction.swift

--- Alare/Models/StopAction.swift
+++ Alare/Models/StopAction.swift
@@ -8,12 +8,21 @@
 import Foundation
 
 enum StopAction: String, CaseIterable, Codable {
+    case waveDevice
+    case scanCode
+    case drumRoll
     case tapButton
     
     static let `default` = tapButton
     
     var displayName: LocalizedStringResource {
         switch self {
+        case .waveDevice:
+            return "Wave Device"
+        case .scanCode:
+            return "Scan Code"
+        case .drumRoll:
+            return "Drum Roll"
         case .tapButton:
             return "Tap Button"
         }
