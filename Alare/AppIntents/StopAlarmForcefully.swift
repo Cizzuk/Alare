7998b204a365e6a3d1f7528eb3a0ebafba9c3289
Cizzuk <dropcat@cizzuk.net>
2026-02-25 18:15:40 +0900

    Add StopAlarmForcefully

---
+++ Alare/AppIntents/StopAlarmForcefully.swift
@@ -0,0 +1,24 @@
+//
+//  StopAlarmForcefully.swift
+//  Alare
+//
+//  Created by Cizzuk on 2026/02/25.
+//
+
+import AppIntents
+import WidgetKit
+
+struct StopAlarmForcefully: AppIntent, CustomIntentMigratedAppIntent {
+    static let intentClassName = "StopAlarmForcefully"
+    static var title: LocalizedStringResource = "Stop Current Alarm and Snooze Forcefully"
+    static var description: LocalizedStringResource = "Even without a Wake-up Action, running this shortcut will forcefully stop the current alarm and snooze."
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        let support = AlarmSupport.shared
+        await support.kill()
+        NotificationCenter.default.post(name: .alarmSettingsDidChangeOutsideMainApp, object: nil)
+        
+        return .result()
+    }
+}
