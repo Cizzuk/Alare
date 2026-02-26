754860beddad463b04005cd8f08eff09cf95f0d7
Cizzuk <dropcat@cizzuk.net>
2026-02-26 21:21:26 +0900

    Add smart stack support

--- WidgetExtension/SnoozeActivityWidget.swift
+++ WidgetExtension/SnoozeActivityWidget.swift
@@ -12,62 +12,84 @@ import WidgetKit
 struct SnoozeActivityWidget: Widget {
     static let kind = "net.cizzuk.alare.WidgetExtension.SnoozeActivityWidget"
     
-    private var largeImage: some View {
-        Image("bolt.alare")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 60, height: 60)
-            .accessibilityLabel("Alare")
-            .foregroundStyle(.dropblue)
-    }
-    
-    private var smallImage: some View {
-        Image("bolt.alare")
-            .resizable()
-            .scaledToFit()
-            .accessibilityLabel("Alare")
-            .foregroundStyle(.dropblue)
-    }
-    
-    private var descriptionText: some View {
-        VStack(alignment: .leading) {
-            Text("Snoozing")
-                .font(.headline)
-                .bold()
+    struct iconImage: View {
+        var size: CGFloat? = nil
+
+        var body: some View {
+            Image("bolt.alare")
+                .resizable()
+                .scaledToFit()
+                .frame(width: size, height: size)
+                .accessibilityLabel("Alare")
                 .foregroundStyle(.dropblue)
-            Text("Start Wake-up Action")
-                .font(.subheadline)
-                .foregroundStyle(.dropblue.opacity(0.8))
+        }
+    }
+    
+    struct descriptionText: View {
+        var showSubtitle: Bool = true
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text("Snoozing")
+                    .font(.headline)
+                    .bold()
+                    .foregroundStyle(.dropblue)
+                if showSubtitle {
+                    Text("Start Wake-up Action")
+                        .font(.subheadline)
+                        .foregroundStyle(.dropblue.opacity(0.8))
+                }
+            }
+        }
+    }
+    
+    struct MainActivityView: View {
+        @Environment(\.activityFamily) var activityFamily
+        
+        var body: some View {
+            switch activityFamily {
+            case .small:
+                HStack(spacing: 10) {
+                    iconImage(size: 30)
+                    descriptionText(showSubtitle: false)
+                }
+            case .medium:
+                HStack(spacing: 10) {
+                    iconImage(size: 50)
+                    descriptionText()
+                }
+                .padding()
+            @unknown default:
+                EmptyView()
+            }
         }
     }
     
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: SnoozeActivityAttributes.self) { context in
-            HStack(spacing: 10) {
-                largeImage
-                descriptionText
-            }
-            .padding()
-            .activitySystemActionForegroundColor(.dropblue)
-            .widgetURL(URL(string: "net.cizzuk.alare://wakeupaction"))
+            MainActivityView()
+                .activitySystemActionForegroundColor(.dropblue)
+                .widgetURL(URL(string: "net.cizzuk.alare://wakeupaction"))
+            
         } dynamicIsland: { context in
             DynamicIsland {
                 DynamicIslandExpandedRegion(.leading) {
-                    largeImage
+                    iconImage(size: 60)
                 }
                 DynamicIslandExpandedRegion(.center) {
-                    descriptionText
+                    descriptionText()
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal, 5)
                 }
             } compactLeading: {
-                smallImage
+                iconImage()
             } compactTrailing: {
             } minimal: {
-                smallImage
+                iconImage()
             }
             .keylineTint(.dropblue)
             .widgetURL(URL(string: "net.cizzuk.alare://wakeupaction"))
         }
+        .supplementalActivityFamilies([.small])
     }
 }
